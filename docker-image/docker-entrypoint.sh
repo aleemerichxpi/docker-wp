6e2ac07e68a4604e48b5df0c093a5f95341f2030
Alessandro Emerich <alessandro.emerich@xpi.com.br>
2020-11-24 20:10:48 -0300

    Add Xdebug

--- docker-image/docker-entrypoint.sh
+++ docker-image/docker-entrypoint.sh
@@ -57,30 +57,30 @@ fi
 
 ! /usr/local/bin/wp-cli.phar cli update --yes
 
-# if [[ -v XDEBUG ]] && [ "$XDEBUG" = "true" ];
-# then
-# 	echo "Using XDEBUG";
+if [[ -v XDEBUG ]] && [ "$XDEBUG" = "true" ];
+then
+	echo "Using XDEBUG";
 
-#     inifile="/usr/local/etc/php/conf.d/pecl-xdebug.ini";
-#     extfile="$(find /usr/local/lib/php/extensions/ -name xdebug.so)";
-#     remote_port="${XDEBUG_IDEKEY:-9000}";
-#     idekey="${XDEBUG_IDEKEY:-xdbg}";
+    inifile="/usr/local/etc/php/conf.d/pecl-xdebug.ini";
+    extfile="$(find /usr/local/lib/php/extensions/ -name xdebug.so)";
+    remote_port="${XDEBUG_IDEKEY:-9000}";
+    idekey="${XDEBUG_IDEKEY:-xdbg}";
 
-#     if [ -f "$extfile" ] && [ ! -f "$inifile" ];
-#     then
-#         {
-#             echo "[Xdebug]";
-#             echo "zend_extension=${extfile}";
-#             echo "xdebug.idekey=${idekey}";
-#             echo "xdebug.remote_enable=on";
-#             echo "xdebug.remote_connect_back=off";
-#             echo "xdebug.remote_autostart=on";
-#             echo "xdebug.remote_host = host.docker.internal";
-#             echo "xdebug.remote_port=${remote_port}";
-#         } > $inifile;
-#     fi
-#     unset extfile remote_port idekey;
-#     echo "XDEBUG configured in 9000 port";
-# fi
+    if [ -f "$extfile" ] && [ ! -f "$inifile" ];
+    then
+        {
+            echo "[Xdebug]";
+            echo "zend_extension=${extfile}";
+            echo "xdebug.idekey=${idekey}";
+            echo "xdebug.remote_enable=on";
+            echo "xdebug.remote_connect_back=off";
+            echo "xdebug.remote_autostart=on";
+            echo "xdebug.remote_host=host.docker.internal";
+            echo "xdebug.remote_port=${remote_port}";
+        } > $inifile;
+    fi
+    unset extfile remote_port idekey;
+    echo "XDEBUG configured in 9000 port";
+fi
 
 exec "$@"
